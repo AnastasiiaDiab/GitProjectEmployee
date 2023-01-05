454fc2bab8dc31bc3050cd448dc0653fe157f4ab
lyaas <lyaastud@gmail.com>
2023-01-05 12:49:51 -0500

    updated Employee

--- src/Employees/Employee.java
+++ src/Employees/Employee.java
@@ -8,6 +8,64 @@ public abstract class Employee {
     private int age;
     private LocalDate DOB;
     private String jobTitle, employeeId;
+
+    public Employee(String name, LocalDate DOB, String jobTitle, String employeeId, double salary) {
+        setName(name);
+        this.age = DOB.getYear() - LocalDate.now().getYear();
+        setDOB(DOB);
+        setJobTitle(jobTitle);
+        setEmployeeId(employeeId);
+        setSalary(salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     private double salary;
 
     public abstract void work();
