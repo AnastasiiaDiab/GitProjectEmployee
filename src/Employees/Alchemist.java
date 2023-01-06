9bce8a6142d9faf04a1160a4d6bc146d7fc7b95e
jule4ka881 <114307410+jule4ka881@users.noreply.github.com>
2023-01-05 22:55:04 -0500

    Update Alchemist.java
    can u except changes, please

--- src/Employees/Alchemist.java
+++ src/Employees/Alchemist.java
@@ -1,7 +1,29 @@
 package Employees;
 
-public class Alchemist {
+public class Alchemist extends Employee implements PartTime{
 
+
+    public Astrologer(String name, LocalDate DOB, String jobTitle, String employeeId, double salary) {
+        super(name, DOB, jobTitle, employeeId, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " " + getName() + " chrysopoeia");
+    }
+
+   
+  
+
+   
+
+    @Override
+    public String toString() {
+        return "Alchemist{" + getName()+
+                "job title =" + getJobTitle()+
+                "salary = " + getSalary()+ "}";
+    }
+}
 
     //new class
 
