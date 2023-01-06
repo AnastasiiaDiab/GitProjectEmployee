e74863ac110d7e4be4cdc3512acbffed7a7f14e2
Alex <4malex@gmail.com>
2023-01-05 22:53:19 -0500

    Alex created object

--- src/Employees/TestObjects.java
+++ src/Employees/TestObjects.java
@@ -13,5 +13,16 @@ public class TestObjects {
 
         emp1.work();
 
+        // ===========================Alex object starts here ================================
+
+        System.out.println();
+
+            Employee employee1 = new SDET("Alex",LocalDate.of(1900, 1, 1), "QA", "QA123",160_000.00);
+
+
+            System.out.println("Alex you are: " + employee1.getAge() + " years old, finally you are SDET and you make a lot of money, like : $" + employee1.getSalary());
+
+        //====================================Alex object code eds here========================
+
     }
 }
